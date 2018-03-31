9b4432a5b8a7cf58c50d478316c01cdedbb7a326
Jennifer Xie <jenniferx@berkeley.edu>
2018-03-31 16:04:19 -0700

    score boxes moved to foreground & something else

---
+++ Assets/Scripts/SimpleTimer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleTimer : MonoBehaviour
+{
+
+    public float targetTime = 60.0f;
+
+    void Update()
+    {
+
+        targetTime -= Time.deltaTime;
+
+        if (targetTime <= 0.0f)
+        {
+            timerEnded();
+        }
+
+    }
+
+    void timerEnded()
+    {
+        //do your stuff here.
+    }
+
+
+}
