9ef466436a655b287a3532258f50c13f14b8faaf
Jennifer Xie <jenniferx@berkeley.edu>
2018-03-31 14:06:08 -0700

    adding all current files

---
+++ Assets/Scripts/AutoclickInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class AutoclickInput : MonoBehaviour
+{
+
+    public InputField inputField;
+
+    // Use this for initialization
+    public void Start()
+    {
+        inputField.ActivateInputField();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
