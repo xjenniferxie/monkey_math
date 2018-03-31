e138b9f857052acee25c2d5e5eedce0a8c738779
ning-hu <ning.hu1205@gmail.com>
2018-03-31 15:20:40 -0700

    added scripts

---
+++ Assets/Scripts/CreateMonkey.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMonkey : MonoBehaviour {
+
+	// Create 1 empty object on left and 1 empty object on right
+	// Attach CreateMonkey script on each
+	// Change startTime to 2 for the one on the right
+	public GameObject Monkey;
+	public int startTime = 3;
+	public float waitTime = 2f;
+	private float InstantiationTimer;
+
+	void Start () {
+
+		InstantiationTimer = waitTime;
+	}
+
+	void Update () {
+
+		CreatePrefab();
+	}
+
+	void CreatePrefab() {
+
+		InstantiationTimer -= Time.deltaTime;
+		if (InstantiationTimer <= 0 && Time.time > startTime)
+		{
+			GameObject monkeyCopy = Instantiate(Monkey, transform.position, Quaternion.identity) as GameObject;
+			monkeyCopy.SetActive (true);
+			InstantiationTimer = waitTime;
+		}
+	}
+}
